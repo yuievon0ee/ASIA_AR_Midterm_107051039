1cbc893566031ba988b1ef7f38d6a9ddab913cbe
yuievon0ee <71679885+yuievon0ee@users.noreply.github.com>
2020-11-07 15:16:47 +0800

    做到一半

---
+++ AR_Midter_107051039/Assets/Scripts/GameManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("安德")]
+    public Transform Slender;
+    [Header("死人骨頭")]
+    public Transform Skeleton;
+    [Header("虛擬搖桿")]
+    public FixedJoystick joystick;
+    [Header("旋轉速度"), Range(0.1f, 20f)]
+    public float turn = 1.5f;
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# ASIA_AR_Midterm_107051039
+ ASIA_AR_Midterm_107051039
